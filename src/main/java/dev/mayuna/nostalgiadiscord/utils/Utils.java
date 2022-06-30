29abdc09b56619af6b4c1c306c01f5778710ad06
lilmayu <uwulilmayu@gmail.com>
2022-06-30 14:00:53 +0200

    Removed trailing space after formatted time

--- src/main/java/dev/mayuna/nostalgiadiscord/utils/Utils.java
+++ src/main/java/dev/mayuna/nostalgiadiscord/utils/Utils.java
@@ -10,11 +10,11 @@ public class Utils {
         long days = hours / 24;
 
         if (days != 0) {
-            return days + "d " + hours % 24 + "h " + minutes % 60 + "m ";
+            return days + "d " + hours % 24 + "h " + minutes % 60 + "m";
         } else if (hours != 0) {
-            return hours % 24 + "h " + minutes % 60 + "m ";
+            return hours % 24 + "h " + minutes % 60 + "m";
         } else if (minutes != 0) {
-            return minutes % 60 + "m ";
+            return minutes % 60 + "m";
         } else if (seconds != 0) {
             return seconds % 60 + "s";
         } else {
